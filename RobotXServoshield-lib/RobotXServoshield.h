f1f054079fe9f37b84bf50356686082d1fc6080f
RobotXSchool <robotx@robotx.su>
2020-10-28 10:36:05 +0300

    add correct defines in header

--- RobotXServoshield-lib/RobotXServoshield.h
+++ RobotXServoshield-lib/RobotXServoshield.h
@@ -1,5 +1,9 @@
+#ifndef _RobotX_Servoshield_H_
+#define _RobotX_Servoshield_H_
+
 #include <Arduino.h>
 
 void servoTurn(uint8_t Set_SerVo, uint16_t timeR, uint8_t sp = 0, uint8_t current = 0);
 void servoReset(uint8_t servoNum);
 void servoInit(uint8_t servoNum,uint16_t startPos=1500);
+#endif //_RobotX_ServoShield_H_
